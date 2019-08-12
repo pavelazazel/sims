126509897ff6e233556853a0edfb9efc8b848ac1
Pavel Azazel <pavelazazel@mail.ru>
2019-08-12 14:54:20 +0300

    add Brand, Type, Name, Location

---
+++ app/controllers/names_controller.rb
@@ -0,0 +1,47 @@
+class NamesController < ApplicationController
+  def index
+    @names = Name.all
+  end
+
+  def show
+    @name = Name.find(params[:id])
+  end
+
+  def new
+    @name = Name.new
+  end
+
+  def edit
+    @name = Name.find(params[:id])
+  end
+
+  def create
+    @name = Name.new(name_params)
+    if @name.save
+      redirect_to names_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @name = Name.find(params[:id])
+    if @name.update(name_params)
+      redirect_to @name
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @name = Name.find(params[:id])
+    @name.destroy
+    redirect_to names_path
+  end
+
+  private
+
+  def name_params
+    params.require(:name).permit(:type_id, :brand_id, :model)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'devices/index'
- 
+
   resources :devices
- 
+  resources :names
+  resources :types
+  resources :brands
+  resources :locations
+
   root 'devices#index'
 end
