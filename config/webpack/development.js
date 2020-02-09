56f278ed72aa8022d5d2dfaa11ce8d2285589c5b
Pavel Azazel <pavelazazel@mail.ru>
2020-02-09 21:45:17 +0300

    fix webpack config

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,15 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+environment.loaders.append("bootstrap.native", {
+  test: /bootstrap\.native/,
+  use: {
+    loader: "bootstrap.native-loader",
+    options: {
+      only: ["modal"],
+      bsVersion: 4
+    }
+  }
+})
+
 module.exports = environment.toWebpackConfig()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,4 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require("webpack")
 
-environment.loaders.append("bootstrap.native", {
-  test: /bootstrap\.native/,
-  use: {
-    loader: "bootstrap.native-loader",
-    options: {
-      only: ["modal"],
-      bsVersion: 4
-    }
-  }
-})
-
 module.exports = environment
